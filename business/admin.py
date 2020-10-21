84e73011537e10420dbce02178d04d07f776dfdc
savsamushaman <kiss_zoltan55@yahoo.com>
2020-10-21 22:06:10 +0300

    commiting before adding new models in case something goes wrong

--- business/admin.py
+++ business/admin.py
@@ -1,4 +1,11 @@
 from django.contrib import admin
 from .models import BusinessModel
 
-admin.site.register(BusinessModel)
+
+class BusinessAdmin(admin.ModelAdmin):
+    list_display = (
+        'business_name', 'manager', 'address', 'is_open_now',
+    )
+
+
+admin.site.register(BusinessModel, BusinessAdmin)
