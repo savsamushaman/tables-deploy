abcae9cfb50caacc7bafba41fa2bf67a3e419b4c
savsamushaman <kiss_zoltan55@yahoo.com>
2021-01-10 17:59:16 +0200

    better websocket opening on the feedpage

--- business/routing.py
+++ business/routing.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .consumers import FeedConsumer
 
 websocket_urlpatterns = [
-    path('ws/owned/<biz_slug>/feed/', FeedConsumer.as_asgi())
+    path('owned/<biz_slug>/feed/', FeedConsumer.as_asgi())
 ]
